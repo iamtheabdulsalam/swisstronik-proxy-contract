4703a6596d6874e78f2d85bf70efe6a22dcf579a
Abdulsalam Abdulsalam <0xdudul@gmail.com>
2024-08-18 15:00:08 +0000

    feat: implemented proxy contract

---
+++ auto.bash
@@ -0,0 +1,22 @@
+npm i
+npm run compile
+
+npm run deploy
+npm run deploy
+npm run deploy
+npm run deploy
+
+npm run initialize
+
+npm run add-issuers
+
+npm run list-issuers
+
+npm run upgrade
+
+
+rm -rf .git
+git init
+git add .
+git commit -m "feat: implemented proxy contract"
+
---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x29e770DEd84C2fa87AcFB88488ceb83e525183c5'
+export const ProxyAdmin = '0xc5d81c36657290c1bf276435e3397C7E09ad11B0'
+export const SWTRImplementation = '0xc24416d817C8336f6E65308A518EFeb7bb94E5C4'
+    
